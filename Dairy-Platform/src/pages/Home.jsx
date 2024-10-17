90ecec8b5e7757537b39c32544cf4e0e92b536f1
mugisha <mohamugi@gmail.com>
2024-10-17 17:53:31 +0300

    Banner component

--- Dairy-Platform/src/pages/Home.jsx
+++ Dairy-Platform/src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import About from "../components/About";
+import Banner from "../components/Banner";
 
 function Home() {
     return (
         <div>
             <Navbar />
+            <Banner/>
             <About/>
             <Footer/>
         </div>
